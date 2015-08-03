60157b364aad3f3b874fa787175f31f394696e75
alcuma <acomisso@keedio.com>
2015-08-03 12:19:46 +0200

    FIX: syntax error in ipa server manifest

--- ambari1/manifests/modules/ipa/manifests/server.pp
+++ ambari1/manifests/modules/ipa/manifests/server.pp
@@ -1,8 +1,6 @@
 class ipa::server {
 
-  package { "ipa-server":
-    ensure => "present",
-  },
+  package { "ipa-server": ensure => "present"}
 
   exec { 'ipa':
   command => "/usr/sbin/ipa-server-install -a adminadmin --hostname=master.ambari.keedio.org -r KEEDIO.ORG -p adminadmin -n ambari.keedio.org -U >& /root/ipa-install.log",
