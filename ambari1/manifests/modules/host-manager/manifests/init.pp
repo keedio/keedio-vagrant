40434a3251b0ae79a9478c6d580b556e5dc63fec
alcuma <acomisso@keedio.com>
2015-09-07 18:05:55 +0200

    KSTCK-71 Reset host-manager to empty state

--- ambari1/manifests/modules/host-manager/manifests/init.pp
+++ ambari1/manifests/modules/host-manager/manifests/init.pp
@@ -1,20 +1,2 @@
 class  host-manager  {
-file_line{'master_line':
-           path => '/etc/hosts',
-           line => '192.168.0.12  master.ambari.keedio.org  master',
-        notify  => Service['ambari-agent'],
-          ensure => 'present',
-          }
-file_line{'ambari1_line':
-           path => '/etc/hosts',
-           line => '192.168.0.10  ambari1.ambari.keedio.org  ambari1',
-        notify  => Service['ambari-agent'],
-          ensure => 'present',
-          }
-file_line{'ambari2_line':
-           path => '/etc/hosts',
-           line => '192.168.0.11  ambari2.ambari.keedio.org  ambari2',
-        notify  => Service['ambari-agent'],
-          ensure => 'present',
-          }
 }
