8ddd9e1dea5ae8b196383d788505ec0149ecad4c
alcuma <acomisso@keedio.com>
2015-10-01 13:00:35 +0200

    BDAAS-17 Added configurable domain and subdomain names

--- ambari1/manifests/modules/ipa/manifests/client.pp
+++ ambari1/manifests/modules/ipa/manifests/client.pp
@@ -1,9 +1,13 @@
 class   ipa::client {
+ $set_domain=hiera(set_domain)
+ $set_subdomain=hiera(set_subdomain)
+ $set_realm=hiera(set_realm)
+ 
  if hiera(ipa) {
  package { "ipa-client": ensure => "installed"}
  package { "ipa-admintools": ensure => "installed"}
  exec { 'ipa':
-  command => "/usr/sbin/ipa-client-install -U --server=master.ambari.keedio.org --domain=ambari.keedio.org  -w adminadmin -p admin",
+  command => "/usr/sbin/ipa-client-install -U --server=master.$set_subdomain.$set_domain  --domain=$set_subdomain.$set_domain  -w adminadmin -p admin",
   creates => "/var/lib/ipa/sysrestore/sysrestore.state",
   require => [Package['ipa-client','ipa-admintools'],Class['base']],
   timeout     => 1200
--- ambari1/manifests/modules/ipa/manifests/server.pp
+++ ambari1/manifests/modules/ipa/manifests/server.pp
@@ -1,10 +1,14 @@
 class ipa::server {
+ $set_domain=hiera(set_domain)
+ $set_subdomain=hiera(set_subdomain)
+ $set_realm=hiera(set_realm)
+
   if hiera(ipa) {
   package { "ipa-server": ensure => "present"}
   package { "bind-dyndb-ldap": ensure => "present"}
 
   exec { 'ipa':
-  command => "/usr/sbin/ipa-server-install -a adminadmin --hostname=master.ambari.keedio.org -r KEEDIO.ORG -p adminadmin -n ambari.keedio.org  -U >& /root/ipa-install.log",
+  command => "/usr/sbin/ipa-server-install -a adminadmin --hostname=master.$set_subdomain.$set_domain -r $set_realm -p adminadmin -n $set_subdomain.$set_domain  -U >& /root/ipa-install.log",
   creates => "/var/lib/ipa/sysrestore/sysrestore.state",
   require => [Package['ipa-server','bind-dyndb-ldap'],Class[base]], 
   timeout     => 1200 
