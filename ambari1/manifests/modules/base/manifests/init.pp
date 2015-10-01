7fefdc2d133734170c50eabcfc5ffa58d5c5395b
alcuma <acomisso@keedio.com>
2015-10-01 12:58:37 +0200

    BDAAS-17 Add diabling repos configuration

--- ambari1/manifests/modules/base/manifests/init.pp
+++ ambari1/manifests/modules/base/manifests/init.pp
@@ -53,29 +53,36 @@ if hiera(nameresolution) == 'script' {
      contain host-manager
  }  
 
+ if hiera(disable_repos) {
+ $isenabled = 0
+ }
+ else {
+ $isenabled = 1
+ }
+
  yumrepo { "epel":
       baseurl => "http://mirror.de.leaseweb.net/epel/6/x86_64",
       descr => "epel",
-      enabled => 0,
+      enabled => $isenabled,
       gpgcheck => 0
   }
 
   yumrepo { "ambari-1.x":
       baseurl => "http://public-repo-1.hortonworks.com/ambari/centos6/1.x/GA",
       descr => "Ambari",
-      enabled => 0,
+      enabled => $isenabled,
       gpgcheck => 0,
   }
   yumrepo { "Updates-ambari-1.x":
       baseurl => "http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0",
       descr => "Ambari",
-      enabled => 0,
+      enabled => $isenabled,
       gpgcheck => 0,
   }
   yumrepo { "ambari-2.0.0Updates":
       baseurl => "http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.0.0",
       descr => "Ambari2",
-      enabled => 0,
+      enabled => $isenabled,
       gpgcheck => 0,
   }
 
@@ -83,7 +90,7 @@ if hiera(nameresolution) == 'script' {
       yumrepo { "epel-apache-maven":
          baseurl => "http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-6Server/x86_64/",
          descr => "maven from apache",
-         enabled => 0,
+         enabled => $isenabled,
          gpgcheck => 0,
   }
   }
