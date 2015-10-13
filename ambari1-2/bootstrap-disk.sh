5f388ded676d1fe343b1533be997462ce4fb753a
alcuma <acomisso@keedio.com>
2015-10-13 09:46:29 +0200

    Removed temporary dir ambari1-2

--- ambari1-2/bootstrap-disk.sh
+++
@@ -1,8 +0,0 @@
-pvdisplay /dev/sdb  
-if [ -b /dev/sdb ] && [ $? == 0 ];
-then
-pvcreate /dev/sdb
-vgextend VolGroup /dev/sdb
-lvextend /dev/VolGroup/lv_root /dev/sdb
-resize2fs /dev/VolGroup/lv_root
-fi
