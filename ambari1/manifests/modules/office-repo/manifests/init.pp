7cdb399795b50fd14f08094bd9f7ab6b5bc7e1a6
Carlos Alvarez <sharlie89@gmail.com>
2017-04-07 12:37:29 +0200

    Removed office-repo manifest

--- ambari1/manifests/modules/office-repo/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-class  office-repo  {
-file_line{'redirect repos to local VM':
-           path => '/etc/hosts',
-           line => '192.168.2.8  repo.keedio.org  repo',
-           ensure => 'present',
-          }
-
-}
