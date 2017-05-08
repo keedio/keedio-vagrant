712f36f0b1784b9e70e09f83c9a1620fed5f782d
Carlos Alvarez <sharlie89@gmail.com>
2017-05-08 17:34:32 +0200

    Fixed location bug

--- ambari1/az_hosts.sh
+++ ambari1/az_hosts.sh
@@ -2,14 +2,17 @@
 
 #This script creates a custom /etc/hosts for Azure Vagrant nodes.
 
+echo "Keedio-Vagrant location:"
+read location
+
 #Get all the IP's internal and public
 
 az vm list-ip-addresses -g "$AZ_RESOURCE_GROUP_NAME" -o table |grep KDS > /tmp/az_hosts
 
-echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" > ~/git/keedio-vagrant/ambari1/files/az_hosts
-echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> ~/git/keedio-vagrant/ambari1/files/az_hosts
+echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" > $location/ambari1/files/az_hosts
+echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> $location/ambari1/files/az_hosts
 
-cat /tmp/az_hosts |awk -F" " '{print $2 "\t" $1".keedio.local""\t" $1}' >> ~/git/keedio-vagrant/ambari1/files/az_hosts
-cat /tmp/az_hosts |awk -F" " '{print $3 "\t" $1".kds.local""\t"}' >> ~/git/keedio-vagrant/ambari1/files/az_hosts
+cat /tmp/az_hosts |awk -F" " '{print $2 "\t" $1".keedio.local""\t" $1}' >> $location/ambari1/files/az_hosts
+cat /tmp/az_hosts |awk -F" " '{print $3 "\t" $1".kds.local""\t"}' >> $location/ambari1/files/az_hosts
 
 rm -f /tmp/az_hosts
