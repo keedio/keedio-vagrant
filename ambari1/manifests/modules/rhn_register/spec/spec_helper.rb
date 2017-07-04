555ad57ba7797f79df5ebd657d649a13116c34cb
Carlos Alvarez <sharlie89@gmail.com>
2017-07-04 09:19:05 +0200

    Removed ambari1

--- ambari1/manifests/modules/rhn_register/spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
-
-RSpec.configure do |c|
-  c.before :each do
-    Puppet.settings[:strict_variables]=true
-  end
-end
--- ambari1/manifests/modules/rhn_register/tests/init.pp
+++
@@ -1,5 +0,0 @@
-class { 'rhn_register':
-  sslca         => '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT',
-  serverurl     => 'https://satellite.example.com/XMLRPC',
-  activationkey => '1-1d6cfafaa90a972e7d3c2743e19f18cc',
-}
